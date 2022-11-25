81eb5df48538d83aaedf7de423cec23b4002e399
Lucca Zaghi <lucca.zaghi@gmail.com>
2022-11-25 12:07:06 -0300

    feat: implementa remoção geral/individual de filtros (req7)

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,26 +3,11 @@ import { HiTrash } from 'react-icons/hi';
 import { NameContext } from '../context/FilterProvider';
 
 function Filter() {
-  // const [inputs, setInputs] = useState({
-  //   column: 'population',
-  //   comparison: 'maior que',
-  //   number: 0,
-  // });
   const [column, setColumn] = useState('population');
   const [comparison, setComparison] = useState('maior que');
   const [number, setNumber] = useState(0);
-  // const [selectOptions, setSelectOptions] = useState([
-  //   'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
-  // ]);
 
   const { setFilteredName, filters, setFilters } = useContext(NameContext);
-
-  // function handleChange({ target }) {
-  //   setInputs({
-  //     ...inputs,
-  //     [target.name]: target.value,
-  //   });
-  // }
 
   function setFilter() {
     setFilters([
@@ -33,17 +18,7 @@ function Filter() {
         number,
       },
     ]);
-
-    console.log(filters);
   }
-
-  // function removeFilter(filter) {
-  //   console.log(filter);
-  //   const newFilters = filters.filter((object) => object.column !== filter.column
-  //     && object.comparison !== filter.comparison && object.number !== filter.number);
-  //   console.log(newFilters);
-  //   setFilters(newFilters);
-  // }
 
   let columnArray = useMemo(() => ([
     'population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water',
@@ -63,6 +38,15 @@ function Filter() {
   useEffect(() => {
     setColumn(columnArray[0]);
   }, [filters, columnArray]);
+
+  function removeAllFilters() {
+    setFilters([]);
+  }
+
+  function removeFilter(filter) {
+    const newFilters = filters.filter((object) => object.column !== filter.column);
+    setFilters(newFilters);
+  }
 
   return (
     <div>
@@ -84,11 +68,6 @@ function Filter() {
             <option key={ idx } value={ col }>{col}</option>
           ))
         }
-        {/* <option value="population">population</option>
-        <option value="orbital_period">orbital_period</option>
-        <option value="diameter">diameter</option>
-        <option value="rotation_period">rotation_period</option>
-        <option value="surface_water">surface_water</option> */}
       </select>
       <select
         data-testid="comparison-filter"
@@ -115,10 +94,26 @@ function Filter() {
         Filtrar
       </button>
       {filters.length > 0
+        && (
+          <button
+            data-testid="button-remove-filters"
+            type="button"
+            onClick={ removeAllFilters }
+          >
+            Remover filtros
+          </button>)}
+      {filters.length > 0
         && filters.map((filter, index) => (
-          <div key={ index }>
-            <span>{`${filter.column} ${filter.comparison} ${filter.number}`}</span>
-            <HiTrash />
+          <div key={ index } data-testid="filter">
+            <span>
+              {`${filter.column} ${filter.comparison} ${filter.number}`}
+            </span>
+            <button
+              type="button"
+              onClick={ () => removeFilter(filter) }
+            >
+              <HiTrash />
+            </button>
           </div>
         ))}
       {console.log(filters.map((filter) => filter.column))}
